bd795cc51981f60c20e6ec8d37e29d38f384c441
Hannah Wang <hannahwang12@gmail.com>
2018-05-30 22:57:45 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import background from './mountains.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="title">ClassWatch.</div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> 
+      </div>
+      
+    );
+  }
+}
+
+export default App;
