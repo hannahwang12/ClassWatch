e3188de33f093fd8ba7f3827835a44f444b28595
Hannah Wang <hannahwang12@gmail.com>
2018-05-31 00:22:04 -0400

    fix file paths

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'style/index.css';
-import App from 'containers/App';
+import './style/index.css';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 // sample branch commit
