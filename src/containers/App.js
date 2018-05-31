818880a48d98097f8f05b5c076a604772e9bbbf8
xcjackpan <xcjackpan99@gmail.com>
2018-05-31 00:13:38 -0400

    file structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import background from './mountains.jpg';
-import './App.css';
+import background from '../assets/img/mountains.jpg';
+import '../style/App.css';
 
 class App extends Component {
   render() {
