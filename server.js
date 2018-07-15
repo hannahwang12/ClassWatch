6b7597f17e155b5e2ea5fae9622b4b680410bce5
xcjackpan <xcjackpan99@gmail.com>
2018-07-15 16:26:20 -0400

    favicon

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const transporter = nodemailer.createTransport({
 	}
 });
 
-
+// Heroku deployment
 server.use(express.static(path.join(__dirname, 'client/build')));
 server.listen(port, () => console.log('Example server up on port 8080'));
 
@@ -73,7 +73,6 @@ server.post("/remove", async (req, res) => {
 	let remove_info = req.body.code.split('|');
 	let del_ref = firebase.app().database().ref().child(remove_info[1]).child(remove_info[2]).child(remove_info[0]);	
 	del_ref.remove();
-	res.send("byte");
 });
 
 server.post('/scrape', async (req, res) => {
@@ -83,7 +82,6 @@ server.post('/scrape', async (req, res) => {
 	const course_number = course_code.match(/\d+./)[0].trim();
 	results = await scraper.go_to_page(term, subject, course_number);
 	em.emit("complete", null); //Emit the event that the get request is listening for
-	res.send("byte");
 });
 
 // When you get a request, call the Promise and send results when it's complete
